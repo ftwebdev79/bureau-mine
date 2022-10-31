bba7e88e6c65fd6e9d03c45a45c36fbcbb614f34
pppbigbud <pppbigbud@gmail.com>
2022-10-31 16:38:31 +0100

    Login ok

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -9,6 +9,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,7 +19,7 @@ class DashboardController extends AbstractDashboardController
     public function __construct(private AdminUrlGenerator $adminUrlGenerator)
     {
     }
-
+    #[isGranted('ROLE_ADMIN')]
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
@@ -41,16 +42,16 @@ class DashboardController extends AbstractDashboardController
     {
         yield MenuItem::section('DashBoard', 'fa fa-home');
 
-        yield MenuItem::subMenu('Products', 'fa-solid fa-newspaper')->setSubItems([
-            MenuItem::linkToCrud('Create artricle', 'fas fa-plus', Article::class)
+        yield MenuItem::subMenu('Articles', 'fa-solid fa-newspaper')->setSubItems([
+            MenuItem::linkToCrud('Creer artricle', 'fas fa-plus', Article::class)
                 ->setAction(Crud::PAGE_NEW),
-            MenuItem::linkToCrud('Show artricle', 'fas fa-eye', Article::class),
+            MenuItem::linkToCrud('Voir les articles', 'fas fa-eye', Article::class),
         ]);
 
         yield MenuItem::subMenu('Categories', 'fa-solid fa-newspaper')->setSubItems([
-            MenuItem::linkToCrud('Create categories', 'fas fa-plus', Category::class)
+            MenuItem::linkToCrud('Creer categories', 'fas fa-plus', Category::class)
                 ->setAction(Crud::PAGE_NEW),
-            MenuItem::linkToCrud('Show categories', 'fas fa-eye', Category::class),
+            MenuItem::linkToCrud('Voir les categories', 'fas fa-eye', Category::class),
         ]);
 
     }
